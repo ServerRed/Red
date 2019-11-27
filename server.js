995f9c533ec0e86c1c3fbdf2eb4d0c087c73f67f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-11-27 11:38:30 +0200

    Simple static files server with path traversal vulnerability

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
+const DIR = process.cwd();
 const HTTP_PORT = 8000;
 const HTTP_HOST = '127.0.0.1';
+const INDEX = 'index.html';
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.end('Hello world server example');
+  let url = decodeURI(req.url);
+  if (url[url.length - 1] === '/') url += INDEX;
+  const fileName = path.join(DIR, 'static', url);
+  console.log({ url, fileName });
+  fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+      res.end('Error 404: File not found');
+      return;
+    }
+    res.end(data);
+  });
 });
 
 server.listen(HTTP_PORT);
